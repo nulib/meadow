141f54c441b6d3f2a2f8af66d53fc00a57725c59
Brendan Quinn <brendan-quinn@northwestern.edu>
2019-06-28 14:38:05 -0500

    Use credo for static code analysis

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule Meadow.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:honeybadger, "~> 0.7"}
+      {:honeybadger, "~> 0.7"},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
     ]
   end
 
