9bdd1fecddb484a6c91f3e1a28c301cfe6d4a436
Brendan Quinn <brendan-quinn@northwestern.edu>
2019-06-28 13:42:08 -0500

    Remove test react components, green up tests

--- assets/js/Root.jsx
+++ assets/js/Root.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Header from './components/Header';
-import CounterPage from './pages/counter';
-import FetchDataPage from './pages/fetch-data';
-import HomePage from './pages';
-import IngestPage from './pages/ingest';
-import CreateIngestProjectPage from './pages/create-ingest-project';
+import Header from "./components/Header";
+import FetchDataPage from "./pages/fetch-data";
+import IngestPage from "./pages/ingest";
+import CreateIngestProjectPage from "./pages/create-ingest-project";
 
 export default class Root extends React.Component {
   render() {
@@ -15,10 +13,8 @@ export default class Root extends React.Component {
         <BrowserRouter>
           <Header />
           <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/counter" component={CounterPage} />
+            <Route path="/" component={IngestPage} />
             <Route path="/fetch-data" component={FetchDataPage} />
-            <Route path="/ingest" component={IngestPage} />
             <Route
               path="/create-ingest-project"
               component={CreateIngestProjectPage}
