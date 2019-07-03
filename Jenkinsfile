db99b166c79a38cfd919e693271ceb516f7b4ea2
Michael Klein <mbklein@gmail.com>
2019-07-03 15:39:00 -0500

    Make migrations work in the release context

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,4 +1,5 @@
 node {
+  def aws_region = "us-east-1"
   def tag_name = env.BRANCH_NAME.split('/').last()
   if ( tag_name == "master" ) {
     tag_name = "production"
@@ -11,5 +12,5 @@ node {
   sh "docker tag \$(docker image ls -q --filter 'label=edu.northwestern.library.stage=deps' --filter 'label=edu.northwestern.library.app=meadow' | head -1) nulib/meadow-deps:${tag_name}"
   sh "docker tag \$(docker image ls -q --filter 'label=edu.northwestern.library.stage=assets' --filter 'label=edu.northwestern.library.app=meadow' | head -1) nulib/meadow-assets:${tag_name}"
   sh "docker image prune -f"
-  sh "docker run -t -v /home/ec2-user/.aws:/root/.aws nulib/awscli --profile ${tag_name} ecs update-service --cluster meadow --service meadow --force-new-deployment"
+  sh "docker run -t -v /home/ec2-user/.aws:/root/.aws -e AWS_DEFAULT_PROFILE=${tag_name} nulib/meadow-deploy"
 }
--- lib/meadow/release_tasks.ex
+++ lib/meadow/release_tasks.ex
@@ -1,89 +1,24 @@
 defmodule Meadow.ReleaseTasks do
-  @moduledoc """
-  Release task to run the database migrations
-  """
-  @start_apps [
-    :crypto,
-    :ssl,
-    :postgrex,
-    :ecto,
-    :ecto_sql,
-    :ecto_ulid
-  ]
+  @app :meadow
 
-  @repos Application.get_env(:meadow, :ecto_repos, [])
-
-  def migrate(_argv) do
-    start_services()
-
-    run_migrations()
-
-    stop_services()
-  end
-
-  def seed(_argv) do
-    start_services()
-
-    run_migrations()
-
-    run_seeds()
-
-    stop_services()
-  end
-
-  defp start_services do
-    IO.puts("Starting dependencies..")
-    # Start apps necessary for executing migrations
-    Enum.each(@start_apps, &Application.ensure_all_started/1)
-
-    # Start the Repo(s) for app
-    IO.puts("Starting repos..")
-
-    # pool_size can be 1 for ecto < 3.0
-    Enum.each(@repos, & &1.start_link(pool_size: 2))
-  end
-
-  defp stop_services do
-    IO.puts("Success!")
-    :init.stop()
-  end
-
-  defp run_migrations do
-    Enum.each(@repos, &run_migrations_for/1)
-  end
-
-  defp run_migrations_for(repo) do
-    app = Keyword.get(repo.config(), :otp_app)
-    IO.puts("Running migrations for #{app}")
-    migrations_path = priv_path_for(repo, "migrations")
-    Ecto.Migrator.run(repo, migrations_path, :up, all: true)
-  end
-
-  defp run_seeds do
-    Enum.each(@repos, &run_seeds_for/1)
-  end
-
-  defp run_seeds_for(repo) do
-    # Run the seed script if it exists
-    seed_script = priv_path_for(repo, "seeds.exs")
-
-    if File.exists?(seed_script) do
-      IO.puts("Running seed script..")
-      Code.eval_file(seed_script)
+  def migrate do
+    for repo <- repos() do
+      create_storage_for(repo)
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
 
-  defp priv_path_for(repo, filename) do
-    app = Keyword.get(repo.config(), :otp_app)
+  defp create_storage_for(repo) do
+    IO.puts :stderr, (case repo.__adapter__.storage_up(repo.config) do
+      :ok -> "The database for #{inspect repo} has been created"
+      {:error, :already_up} -> "The database for #{inspect repo} has already been created"
+      {:error, term} when is_binary(term) -> "The database for #{inspect repo} couldn't be created: #{term}"
+      {:error, term} -> "The database for #{inspect repo} couldn't be created: #{inspect term}"
+    end)
+  end
 
-    repo_underscore =
-      repo
-      |> Module.split()
-      |> List.last()
-      |> Macro.underscore()
-
-    priv_dir = "#{:code.priv_dir(app)}"
-
-    Path.join([priv_dir, repo_underscore, filename])
+  defp repos do
+    Application.load(@app)
+    Application.fetch_env!(@app, :ecto_repos)
   end
 end
