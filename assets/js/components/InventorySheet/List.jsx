91459f0e76d16b162e9e3cb83405fd5dc07e4838
Adam Arling <adam.arling@northwestern.edu>
2019-07-19 15:11:19 -0400

    Connect real inventory sheet list to Project page.  Stubbed out inventory sheet display page.

--- assets/js/components/InventorySheet/List.jsx
+++ assets/js/components/InventorySheet/List.jsx
@@ -1,19 +1,30 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { mockInventorySheets } from "../../mock-data/inventorySheets";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const InventorySheetList = ({ projectId }) => {
   const [inventorySheets, setInventorySheets] = useState([]);
+  const url = "/api/v1/ingest_jobs";
 
   useEffect(() => {
-    // Get inventory sheets for project based on id
-    getInventorySheets();
-  });
+    // Start it off by assuming the component is still mounted
+    let mounted = true;
 
-  function getInventorySheets() {
-    setInventorySheets(mockInventorySheets);
-  }
+    const loadData = async () => {
+      const response = await axios.get(url);
+      // We have a response, but let's first check if component is still mounted
+      if (mounted) {
+        setInventorySheets(response.data.data);
+      }
+    };
+    loadData();
+
+    return () => {
+      // When cleanup is called, toggle the mounted variable to false
+      mounted = false;
+    };
+  }, [url]);
 
   return (
     <div>
@@ -29,12 +40,13 @@ const InventorySheetList = ({ projectId }) => {
             <li key={sheet.id} className="pb-4">
               <p>
                 <Link to={`/project/${projectId}/inventory-sheet/${sheet.id}`}>
-                  {sheet.title}
+                  {sheet.name}
                 </Link>
               </p>
-              <p>Total Works: {sheet.totalWorks}</p>
-              <p>Ingested: {sheet.dateCreated}</p>
-              <p>Creator: {sheet.creator}</p>
+              <p>Filename: ${sheet.filename}</p>
+              <p>Total Works: xxx</p>
+              <p>Ingested: 2019-05-12</p>
+              <p>Creator: Some person</p>
             </li>
           ))}
         </ul>
