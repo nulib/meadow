5324f34f39ac89fe4628f3852243250671acff9e
Michael B. Klein <mbklein@gmail.com>
2019-08-29 16:11:33 -0500

    Set remsh vars and open port 4369

--- terraform/main.tf
+++ terraform/main.tf
@@ -144,6 +144,15 @@ resource "aws_security_group_rule" "allow_alb_access" {
   security_group_id = "${aws_security_group.meadow.id}"
 }
 
+resource "aws_security_group_rule" "allow_alb_remsh_access" {
+  type              = "ingress"
+  from_port         = 4369
+  to_port           = 4369
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.meadow.id}"
+}
+
 data "aws_route53_zone" "app_zone" {
   name = "${var.dns_zone}"
 }
