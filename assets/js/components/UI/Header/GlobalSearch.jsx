e8fb783e954e5878079939afd82c2b74505dbb13
Adam Arling <adam.arling@northwestern.edu>
2019-10-02 09:27:37 -0500

    Add some more front-end testing setup config, plus some additional leaf level tests and patterns to follow in the future.

--- assets/js/components/UI/Header/GlobalSearch.jsx
+++ assets/js/components/UI/Header/GlobalSearch.jsx
@@ -4,18 +4,15 @@ import SearchIcon from "../../../../css/fonts/zondicons/search.svg";
 const UIHeaderGlobalSearch = ({ currentUser }) => (
   <div className="w-full lg:px-6 md:w-2/3 xl:px-12">
     <div className="relative">
-      {currentUser && (
-        <>
-          <input
-            type="text"
-            placeholder="Search for items"
-            className="transition focus:outline-0 text-gray-700 border border-transparent focus:bg-white focus:border-gray-300 placeholder-gray-500 rounded-lg bg-gray-200 py-2 pr-4 pl-10 block w-full appearance-none leading-tight ds-input"
-          />
-          <div className="pointer-events-none absolute inset-y-0 left-0 pl-4 flex items-center">
-            <SearchIcon width={50} height={50} className="icon" />
-          </div>
-        </>
-      )}
+      <input
+        data-testid="global-search"
+        type="text"
+        placeholder="Search for items"
+        className="transition focus:outline-0 text-gray-700 border border-transparent focus:bg-white focus:border-gray-300 placeholder-gray-500 rounded-lg bg-gray-200 py-2 pr-4 pl-10 block w-full appearance-none leading-tight ds-input"
+      />
+      <div className="pointer-events-none absolute inset-y-0 left-0 pl-4 flex items-center">
+        <SearchIcon width={50} height={50} className="icon" />
+      </div>
     </div>
   </div>
 );
