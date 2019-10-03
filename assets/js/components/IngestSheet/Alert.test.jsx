6b5161aa69c94e3267e74aefe6858dab509945ff
Karen Shaw Didrickson <karendid@gmail.com>
2019-10-03 13:55:08 -0500

    Additional alerts for ingest sheet status and tests

--- assets/js/components/IngestSheet/Alert.test.jsx
+++ assets/js/components/IngestSheet/Alert.test.jsx
@@ -33,8 +33,6 @@ it("displays danger alert with file fail message when the Ingest Sheet status is
   const { debug, getByTestId, getByText } = render(
     <IngestSheetAlert ingestSheet={fileFail} />
   );
-  // This will show you the rendered UI output, which is helpful to debug or see what's going on.  Remove before committing.
-  debug();
 
   const alertElement = getByTestId("ui-alert");
 
@@ -44,4 +42,209 @@ it("displays danger alert with file fail message when the Ingest Sheet status is
   expect(
     getByText("Invalid csv file: unexpected escape character...")
   ).toBeInTheDocument();
+});
+
+const rowFail = {
+  fileErrors: ["Invalid header: something"],
+  filename: "s3://dev-uploads/ingest_sheets/01DP6H30V4XV3Z5W4H782N2BXM.csv",
+  id: "01DP6H3JWC6VGE7EH5KW1GHNQZ",
+  ingestSheetRows: [],
+  name: "Illegal header",
+  state: [
+    {
+      name: "file",
+      state: "PASS"
+    },
+    {
+      name: "rows",
+      state: "FAIL"
+    },
+    {
+      name: "overall",
+      state: "FAIL"
+    }
+  ],
+  status: "ROW_FAIL"
+};
+
+it("renders without crashing", () => {
+  const { debug } = render(<IngestSheetAlert />);
+});
+
+it("displays danger alert with row fail message when the Ingest Sheet status is ROW_FAIL", () => {
+  const { debug, getByTestId, getByText } = render(
+    <IngestSheetAlert ingestSheet={rowFail} />
+  );
+
+  const alertElement = getByTestId("ui-alert");
+
+  expect(alertElement).toBeInTheDocument();
+  expect(alertElement).toHaveClass("danger");
+  expect(getByText("File has failing rows")).toBeInTheDocument();
+});
+
+const valid = {
+  fileErrors: [],
+  filename: "s3://dev-uploads/ingest_sheets/01DP6H30V4XV3Z5W4H782N2BXM.csv",
+  id: "01DP6H3JWC6VGE7EH5KW1GHNQZ",
+  ingestSheetRows: [],
+  name: "Valid File",
+  state: [
+    {
+      name: "file",
+      state: "PASS"
+    },
+    {
+      name: "rows",
+      state: "PASS"
+    },
+    {
+      name: "overall",
+      state: "PASS"
+    }
+  ],
+  status: "VALID"
+};
+
+it("renders without crashing", () => {
+  const { debug } = render(<IngestSheetAlert />);
+});
+
+it("displays success alert with valid file message when the Ingest Sheet status is VALID", () => {
+  const { debug, getByTestId, getByText } = render(
+    <IngestSheetAlert ingestSheet={valid} />
+  );
+
+  const alertElement = getByTestId("ui-alert");
+
+  expect(alertElement).toBeInTheDocument();
+  expect(alertElement).toHaveClass("success");
+  expect(getByText("File is valid")).toBeInTheDocument();
+  expect(
+    getByText("All checks have passed and the ingest sheet is valid.")
+  ).toBeInTheDocument();
+});
+
+const approved = {
+  fileErrors: [],
+  filename: "s3://dev-uploads/ingest_sheets/01DP6H30V4XV3Z5W4H782N2BXM.csv",
+  id: "01DP6H3JWC6VGE7EH5KW1GHNQZ",
+  ingestSheetRows: [],
+  name: "Approved File",
+  state: [
+    {
+      name: "file",
+      state: "PASS"
+    },
+    {
+      name: "rows",
+      state: "PASS"
+    },
+    {
+      name: "overall",
+      state: "PASS"
+    }
+  ],
+  status: "APPROVED"
+};
+
+it("renders without crashing", () => {
+  const { debug } = render(<IngestSheetAlert />);
+});
+
+it("displays info alert with approved message when the Ingest Sheet status is APPROVED", () => {
+  const { debug, getByTestId, getByText } = render(
+    <IngestSheetAlert ingestSheet={approved} />
+  );
+
+  const alertElement = getByTestId("ui-alert");
+
+  expect(alertElement).toBeInTheDocument();
+  expect(alertElement).toHaveClass("info");
+  expect(getByText("Approved")).toBeInTheDocument();
+  expect(
+    getByText(
+      "The Ingest Sheet has been approved and the ingest is in progress"
+    )
+  ).toBeInTheDocument();
+});
+
+const completed = {
+  fileErrors: [],
+  filename: "s3://dev-uploads/ingest_sheets/01DP6H30V4XV3Z5W4H782N2BXM.csv",
+  id: "01DP6H3JWC6VGE7EH5KW1GHNQZ",
+  ingestSheetRows: [],
+  name: "Completed File",
+  state: [
+    {
+      name: "file",
+      state: "PASS"
+    },
+    {
+      name: "rows",
+      state: "PASS"
+    },
+    {
+      name: "overall",
+      state: "PASS"
+    }
+  ],
+  status: "COMPLETED"
+};
+
+it("renders without crashing", () => {
+  const { debug } = render(<IngestSheetAlert />);
+});
+
+it("displays success alert with completed message when the Ingest Sheet status is COMPLETED", () => {
+  const { debug, getByTestId, getByText } = render(
+    <IngestSheetAlert ingestSheet={completed} />
+  );
+
+  const alertElement = getByTestId("ui-alert");
+
+  expect(alertElement).toBeInTheDocument();
+  expect(alertElement).toHaveClass("success");
+  expect(getByText("Ingestion Complete")).toBeInTheDocument();
+  expect(getByText("All files have been processed")).toBeInTheDocument();
+});
+
+const deleted = {
+  fileErrors: [],
+  filename: "s3://dev-uploads/ingest_sheets/01DP6H30V4XV3Z5W4H782N2BXM.csv",
+  id: "01DP6H3JWC6VGE7EH5KW1GHNQZ",
+  ingestSheetRows: [],
+  name: "Deleted File",
+  state: [
+    {
+      name: "file",
+      state: "PASS"
+    },
+    {
+      name: "rows",
+      state: "PASS"
+    },
+    {
+      name: "overall",
+      state: "PASS"
+    }
+  ],
+  status: "DELETED"
+};
+
+it("renders without crashing", () => {
+  const { debug } = render(<IngestSheetAlert />);
+});
+
+it("displays success alert with completed message when the Ingest Sheet status is COMPLETED", () => {
+  const { debug, getByTestId, getByText } = render(
+    <IngestSheetAlert ingestSheet={deleted} />
+  );
+
+  const alertElement = getByTestId("ui-alert");
+
+  expect(alertElement).toBeInTheDocument();
+  expect(alertElement).toHaveClass("danger");
+  expect(getByText("Deleted")).toBeInTheDocument();
+  expect(getByText("Ingest sheet no longer exists")).toBeInTheDocument();
 });
