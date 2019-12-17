cdc425cab4cef2553a435caa3dd5c1cec00e8259
Karen Shaw Didrickson <karendid@gmail.com>
2019-12-17 08:58:48 -0600

    Progress bar text is percentage complete

--- priv/bin/fake_pyramid.js
+++ priv/bin/fake_pyramid.js
@@ -3,24 +3,22 @@
 process.stdin.resume();
 
 process.stdin.on("data", data => {
-  sleep(5000).then(() => {
-    var r = Math.floor(Math.random() * 5);
-    switch (r) {
-      case 1:
-        const parsedJSON = JSON.parse(data);
-        const target = parsedJSON.target;
-        process.stdout.write(`${target} not found`);
-        break;
-      case 2:
-        process.exit(1);
-        break;
-      case 3:
-        break;
-      default:
-        process.stdout.write("complete");
-        break;
-    }
-  });
+  var r = Math.floor(Math.random() * 8);
+  switch (r) {
+    case 1:
+      const parsedJSON = JSON.parse(data);
+      const target = parsedJSON.target;
+      process.stdout.write(`${target} not found`);
+      break;
+    case 2:
+      process.exit(1);
+      break;
+    case 3:
+      break;
+    default:
+      process.stdout.write("complete");
+      break;
+  }
 });
 process.stdin.on("end", () => {
   process.exit;
@@ -29,7 +27,3 @@ process.stdin.on("end", () => {
 process.stdin.on("exit", () => {
   process.exit;
 });
-
-const sleep = milliseconds => {
-  return new Promise(resolve => setTimeout(resolve, milliseconds));
-};
