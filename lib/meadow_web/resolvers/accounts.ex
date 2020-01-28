f4b6a0257e32592f5ebbf4dc177a9424105cec08
Karen Shaw Didrickson <karendid@gmail.com>
2020-01-28 14:44:07 -0600

    Return mock data from API

--- lib/meadow_web/resolvers/accounts.ex
+++ lib/meadow_web/resolvers/accounts.ex
@@ -2,6 +2,15 @@ defmodule MeadowWeb.Resolvers.Accounts do
   @moduledoc """
   Absinthe resolver for User and Group Management related functionality
   """
+  defmodule Entry do
+    @moduledoc """
+    Temporary struct for mocked data
+    """
+    defstruct [:id, :name, :type]
+  end
+
+  alias Meadow.Accounts.Ldap
+  alias MeadowWeb.Resolvers.Accounts
 
   def me(_, _, %{context: %{current_user: user}}) do
     {:ok, user}
@@ -9,5 +18,83 @@ defmodule MeadowWeb.Resolvers.Accounts do
 
   def me(_, _, _) do
     {:ok, nil}
+  end
+
+  def list_roles(_, _, _) do
+    {:ok,
+     [
+       %Entry{
+         id: "CN=Administrators,OU=Meadow,DC=library,DC=northwestern,DC=edu",
+         name: "Administrators",
+         type: "group"
+       },
+       %Entry{
+         id: "CN=Managers,OU=Meadow,DC=library,DC=northwestern,DC=edu",
+         name: "Managers",
+         type: "group"
+       },
+       %Entry{
+         id: "CN=Editors,OU=Meadow,DC=library,DC=northwestern,DC=edu",
+         name: "Editors",
+         type: "group"
+       },
+       %Entry{
+         id: "CN=Users,OU=Meadow,DC=library,DC=northwestern,DC=edu",
+         name: "Users",
+         type: "group"
+       }
+     ]}
+  end
+
+  def role_members(_, %{id: id}, _) do
+    {:ok,
+     [
+       %Entry{
+         id: "CN=DevOps,OU=Meadow,DC=library,DC=northwestern,DC=edu",
+         name: "DevOps",
+         type: "group"
+       },
+       %Entry{
+         id: "CN=Curators,OU=Meadow,DC=library,DC=northwestern,DC=edu",
+         name: "Curators",
+         type: "group"
+       },
+       %Entry{
+         id: "CN=Temp Workers,OU=Meadow,DC=library,DC=northwestern,DC=edu",
+         name: "Temp Workers",
+         type: "group"
+       },
+       %Entry{
+         id: "CN=jad123,OU=Meadow,DC=library,DC=northwestern,DC=edu",
+         name: "Jane Doe",
+         type: "user"
+       }
+     ]}
+  end
+
+  def group_members(_, %{id: id}, _) do
+    {:ok,
+     [
+       %Entry{
+         id: "CN=dld978,OU=Meadow,DC=library,DC=northwestern,DC=edu",
+         name: "Darryl Duggan",
+         type: "user"
+       },
+       %Entry{
+         id: "CN=mtp654,OU=Meadow,DC=library,DC=northwestern,DC=edu",
+         name: "Myah Parks",
+         type: "user"
+       },
+       %Entry{
+         id: "CN=RDC Staff,OU=Meadow,DC=library,DC=northwestern,DC=edu",
+         name: "RDC Staff",
+         type: "group"
+       },
+       %Entry{
+         id: "CN=jad123,OU=Meadow,DC=library,DC=northwestern,DC=edu",
+         name: "Jane Doe",
+         type: "user"
+       }
+     ]}
   end
 end
