b261c294b3f57a539c0b01a14b39028aeb073bb4
Michael B. Klein <mbklein@gmail.com>
2020-02-13 12:16:41 -0600

    Make sure embedded nils are always converted to an empty map of the right type by the changeset
    Co-Authored-By: Brendan Quinn <brendan-quinn@northwestern.edu>

---
+++ lib/meadow/data/schemas/validations.ex
@@ -0,0 +1,24 @@
+defmodule Meadow.Data.Schemas.Validations do
+  @moduledoc """
+  This module provides custom changeset functions and
+  validations
+  """
+
+  @doc """
+  If a `cast_embed()` will result in a `nil` value (either on create or
+  update), set it to an empty embedded struct instead
+  """
+  def prepare_embed(%Ecto.Changeset{data: data, params: params} = change, field) do
+    with f <- to_string(field) do
+      case {Map.get(params, f), Map.get(data, f)} do
+        {nil, nil} ->
+          {:embed, field_spec} = change.data.__struct__.__schema__(:type, field)
+          params = Map.put(params, f, field_spec.related.__struct__ |> Map.from_struct())
+          Map.put(change, :params, params)
+
+        _ ->
+          change
+      end
+    end
+  end
+end
