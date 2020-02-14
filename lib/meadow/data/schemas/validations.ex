608e96f5cff2d7e3712297bbf4c105273c906665
Brendan Quinn <brendan-quinn@northwestern.edu>
2020-02-14 13:41:40 -0600

    Add is_atom/1 guard clause to Meadow.Data.Schemas.Validations.prepare_embed/2

--- lib/meadow/data/schemas/validations.ex
+++ lib/meadow/data/schemas/validations.ex
@@ -8,7 +8,7 @@ defmodule Meadow.Data.Schemas.Validations do
   If a `cast_embed()` will result in a `nil` value (either on create or
   update), set it to an empty embedded struct instead
   """
-  def prepare_embed(%Ecto.Changeset{data: data, params: params} = change, field) do
+  def prepare_embed(%Ecto.Changeset{data: data, params: params} = change, field) when is_atom(field) do
     with f <- to_string(field) do
       case {Map.get(params, f), Map.get(data, f)} do
         {nil, nil} ->
