c8cd67bba9c454059207c3c3b15fa96bc84c4e8f
Adam Arling <adam.arling@northwestern.edu>
2020-02-25 11:02:41 -0600

    Initial setup of Work screen tabs EDIT mode structure

---
+++ assets/js/hooks/useIsEditing.js
@@ -0,0 +1,11 @@
+import { useState, useCallback } from "react";
+
+export default function useIsEditing(initialValue) {
+  const [isEditing, setIsEditing] = useState(initialValue);
+  return [
+    isEditing,
+    useCallback(value => {
+      return setIsEditing(value);
+    })
+  ];
+}
