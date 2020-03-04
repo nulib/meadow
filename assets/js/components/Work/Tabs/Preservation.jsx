e27069b106442142539aff98f065597367609fb1
Divya Katpally <katdivyareddy@gmail.com>
2020-03-04 12:23:30 -0600

    Changes to expose preservation work data in the tab

--- assets/js/components/Work/Tabs/Preservation.jsx
+++ assets/js/components/Work/Tabs/Preservation.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import UITagNotYetSupported from "../../UI/TagNotYetSupported";
 
 const WorkTabsPreservation = ({ work }) => {
   return (
     <div className="columns is-centered">
       <div className="column">
-        <table className="table is-fullwidth is-striped is-hoverable">
+        <table className="table is-fullwidth is-striped is-hoverable is-fixed">
           <thead>
             <tr>
               <th>Role</th>
@@ -24,15 +25,17 @@ const WorkTabsPreservation = ({ work }) => {
                 return (
                   <tr key={fileset.id}>
                     <td>{fileset.role}</td>
-                    <td>{metadata ? metadata.originalFilename : " "}</td>
-                    <td className="notification is-warning">
-                      {metadata.sha256}
+                    <td className="break-word">
+                      {metadata ? metadata.originalFilename : " "}
                     </td>
-                    <td className="notification is-warning">
-                      TODO: Need this exposed in GraphQL
+                    <td className="break-word">
+                      {metadata ? metadata.sha256 : ""}
                     </td>
-                    <td className="notification is-warning">
-                      TODO: Need this exposed in GraphQL
+                    <td className="break-word">
+                      {metadata ? metadata.location : ""}
+                    </td>
+                    <td>
+                      <UITagNotYetSupported label="Display not yet supported" />
                     </td>
                     <td>
                       <div className="buttons-end">
