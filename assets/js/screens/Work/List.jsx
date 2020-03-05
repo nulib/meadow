43ebabb05499cde841a02691a7d7e0c6fac9f0c7
Karen Shaw Didrickson <karendid@gmail.com>
2020-03-05 10:41:02 -0600

    Remove hard coded dev  iiif server url

--- assets/js/screens/Work/List.jsx
+++ assets/js/screens/Work/List.jsx
@@ -3,47 +3,50 @@ import Layout from "../Layout";
 import Loading from "../../components/UI/Loading";
 import SearchResultItem from "../../components/Search/ResultItem";
 import { ReactiveList } from "@appbaseio/reactivesearch";
+import { IIIFProvider } from "../../components/IIIF/IIIFProvider";
 
 const ScreensWorkList = () => {
   return (
     <Layout>
       <section className="section">
         <div className="container">
-          <ReactiveList
-            componentId="SearchResult"
-            dataField="accession_number"
-            defaultQuery={() => ({
-              query: {
-                bool: {
-                  must: [
-                    {
-                      match: {
-                        "model.name": "Image"
+          <IIIFProvider>
+            <ReactiveList
+              componentId="SearchResult"
+              dataField="accession_number"
+              defaultQuery={() => ({
+                query: {
+                  bool: {
+                    must: [
+                      {
+                        match: {
+                          "model.name": "Image"
+                        }
                       }
-                    }
-                  ]
+                    ]
+                  }
                 }
-              }
-            })}
-            innerClass={{
-              list: "columns is-multiline",
-              resultStats: "is-size-6 has-text-grey"
-            }}
-            react={{
-              and: ["SearchSensor"]
-            }}
-            renderItem={res => {
-              return (
-                <div
-                  key={res._id}
-                  className="column is-half-tablet is-one-third-desktop is-one-quarter-widescreen"
-                >
-                  <SearchResultItem res={res} />
-                </div>
-              );
-            }}
-            showResultStats={true}
-          />
+              })}
+              innerClass={{
+                list: "columns is-multiline",
+                resultStats: "is-size-6 has-text-grey"
+              }}
+              react={{
+                and: ["SearchSensor"]
+              }}
+              renderItem={res => {
+                return (
+                  <div
+                    key={res._id}
+                    className="column is-half-tablet is-one-third-desktop is-one-quarter-widescreen"
+                  >
+                    <SearchResultItem res={res} />
+                  </div>
+                );
+              }}
+              showResultStats={true}
+            />
+          </IIIFProvider>
         </div>
       </section>
     </Layout>
