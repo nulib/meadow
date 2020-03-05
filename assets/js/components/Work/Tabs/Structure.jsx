c83bb159f9138f48ef80bac02fcea6b6064a4e1d
Adam Arling <adam.arling@northwestern.edu>
2020-03-05 11:14:20 -0600

    Wire up setWorkImage UI in Work Structure tab

--- assets/js/components/Work/Tabs/Structure.jsx
+++ assets/js/components/Work/Tabs/Structure.jsx
@@ -1,18 +1,43 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import PropTypes from "prop-types";
 import { IIIFContext } from "../../IIIF/IIIFProvider";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import useIsEditing from "../../../hooks/useIsEditing";
 import { useForm } from "react-hook-form";
+import { useMutation } from "@apollo/react-hooks";
+import { SET_WORK_IMAGE } from "../work.query";
+import { toastWrapper } from "../../../services/helpers";
+
+const parseWorkRepresentativeImage = work => {
+  if (!work.representativeImage) return;
+  const arr = work.representativeImage.split("/");
+  return arr[arr.length - 1];
+};
 
 const WorkTabsStructure = ({ work }) => {
   const [isEditing, setIsEditing] = useIsEditing();
+  const [workImageFilesetId, setWorkImageFilesetId] = useState(
+    parseWorkRepresentativeImage(work)
+  );
   const { register, handleSubmit, errors } = useForm();
   const iiifServerUrl = useContext(IIIFContext);
+
+  const [setWorkImage] = useMutation(SET_WORK_IMAGE, {
+    onCompleted({ setWorkImage }) {
+      toastWrapper("is-success", "Work image has been updated");
+    }
+  });
 
   if (!work) {
     return null;
   }
+
+  const handleWorkImageChange = id => {
+    if (id === workImageFilesetId) return;
+
+    setWorkImageFilesetId(id === workImageFilesetId ? null : id);
+    setWorkImage({ variables: { fileSetId: id, workId: work.id } });
+  };
 
   const onSubmit = data => {
     // TODO : add logic to update filesets for given work.
@@ -26,107 +51,112 @@ const WorkTabsStructure = ({ work }) => {
           {work.fileSets.map(({ id, accessionNumber, metadata }) => (
             <article key={id} className="media">
               <figure className="media-left">
-                <p className="image is-64x64">
+                <p className="image is-128x128">
                   <img
                     src={`${iiifServerUrl}${id}/square/500,500/0/default.jpg`}
                     placeholder="Fileset Image"
-                    onError={e => {
-                      e.target.src = "/images/1280x960.png";
-                    }}
                   />
                 </p>
               </figure>
               <div className="media-content">
                 <div className="content">
-                  {isEditing ? (
-                    <>
-                      <div className="field">
-                        <strong>{accessionNumber}</strong>
-                      </div>
-                      <div className="field">
-                        <div className="field is-horizontal">
-                          <div className="field-label is-normal">
-                            <label className="label">Label</label>
-                          </div>
-                          <div className="field-body">
-                            <div className="field">
-                              <p className="control">
-                                <input
-                                  ref={register({ required: true })}
-                                  name={`label-${id}`}
-                                  data-testid="input-label"
-                                  className={`input ${
-                                    errors[`label-${id}`] ? "is-danger" : ""
-                                  }`}
-                                  type="text"
-                                  placeholder="Label"
-                                  defaultValue={metadata.label}
-                                />
-                              </p>
-                              {errors[`label-${id}`] && (
-                                <p className="help is-danger">
-                                  Label is required
-                                </p>
-                              )}
-                            </div>
-                          </div>
-                        </div>
-                      </div>
+                  <p>
+                    <span className="tag is-medium">
+                      Accession Number: {accessionNumber}
+                    </span>
+                  </p>
 
-                      <div className="field">
-                        <div className="field is-horizontal">
-                          <div className="field-label is-normal">
-                            <label className="label">Description</label>
-                          </div>
-                          <div className="field-body">
-                            <div className="field">
-                              <p className="control">
-                                <textarea
-                                  ref={register({ required: true })}
-                                  name={`metadataDescription-${id}`}
-                                  data-testid="textarea-metadata-description"
-                                  className={`input ${
-                                    errors[`metadataDescription-${id}`]
-                                      ? "is-danger"
-                                      : ""
-                                  }`}
-                                  defaultValue={metadata.description}
-                                  type="text"
-                                ></textarea>
-                              </p>
-                              {errors[`metadataDescription-${id}`] && (
-                                <p className="help is-danger">
-                                  Metadata Description is required
-                                </p>
-                              )}
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </>
-                  ) : (
-                    <p>
-                      <strong>{accessionNumber}</strong>
-                      <br />
-                      {metadata.label}
-                      <br />
-                      {metadata.description}
-                    </p>
-                  )}
+                  <div className="field">
+                    <label className="label">Label</label>
+                    <div className="control">
+                      {isEditing ? (
+                        <input
+                          ref={register({ required: true })}
+                          name={`label-${id}`}
+                          data-testid="input-label"
+                          className={`input ${
+                            errors[`label-${id}`] ? "is-danger" : ""
+                          }`}
+                          type="text"
+                          placeholder="Label"
+                          defaultValue={metadata.label}
+                        />
+                      ) : (
+                        <p>{metadata.label}</p>
+                      )}
+                    </div>
+                    {errors[`label-${id}`] && (
+                      <p className="help is-danger">Label is required</p>
+                    )}
+                  </div>
+
+                  <div className="field">
+                    <label className="label">Description</label>
+                    <div className="control">
+                      {isEditing ? (
+                        <textarea
+                          ref={register({ required: true })}
+                          name={`metadataDescription-${id}`}
+                          data-testid="textarea-metadata-description"
+                          className={`input ${
+                            errors[`metadataDescription-${id}`]
+                              ? "is-danger"
+                              : ""
+                          }`}
+                          defaultValue={metadata.description}
+                          type="text"
+                        ></textarea>
+                      ) : (
+                        <p>{metadata.description}</p>
+                      )}
+                    </div>
+                    {errors[`metadataDescription-${id}`] && (
+                      <p className="help is-danger">
+                        Metadata Description is required
+                      </p>
+                    )}
+                  </div>
                 </div>
               </div>
-              {isEditing ? (
-                ""
-              ) : (
-                <div className="media-right">
-                  <button className="button">
-                    <FontAwesomeIcon icon="file-download" /> .tiff
-                  </button>
-                  <button className="button">
-                    <FontAwesomeIcon icon="file-download" /> .jpg
-                  </button>
-                </div>
-              )}
+              <div className="media-right has-text-right">
+                {isEditing ? (
+                  ""
+                ) : (
+                  <>
+                    <div className="field">
+                      <input
+                        id={`checkbox-work-switch-${id}`}
+                        type="checkbox"
+                        name={`checkbox-work-switch-${id}`}
+                        className="switch"
+                        checked={workImageFilesetId === id}
+                        onChange={e => handleWorkImageChange(id)}
+                      />
+                      <label htmlFor={`checkbox-work-switch-${id}`}>
+                        Work image
+                      </label>
+                    </div>
+                    <div className="field has-addons">
+                      <p className="control">
+                        <button className="button">
+                          <span className="icon">
+                            <FontAwesomeIcon icon="file-download" />
+                          </span>{" "}
+                          <span>TIFF</span>
+                        </button>
+                      </p>
+                      <p className="control">
+                        <button className="button">
+                          <span className="icon">
+                            <FontAwesomeIcon icon="file-download" />
+                          </span>{" "}
+                          <span>JPG</span>
+                        </button>
+                      </p>
+                    </div>
+                  </>
+                )}
+              </div>
             </article>
           ))}
 
@@ -175,7 +205,7 @@ const WorkTabsStructure = ({ work }) => {
                 </button>
                 <button
                   type="button"
-                  className="button"
+                  className="button is-text"
                   onClick={() => setIsEditing(false)}
                 >
                   Cancel
