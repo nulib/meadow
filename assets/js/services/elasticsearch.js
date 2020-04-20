4a52f42e504490a2685ce0bef39aceb9de0b0a27
Michael B. Klein <mbklein@gmail.com>
2020-04-20 11:36:42 -0500

    Include port in elasticsearch proxy endpoint.

--- assets/js/services/elasticsearch.js
+++ assets/js/services/elasticsearch.js
@@ -1,6 +1,7 @@
 const protocol = location.protocol;
 const slashes = protocol.concat("//");
 const host = slashes.concat(window.location.hostname);
+const port = window.location.port;
 
-export const ELASTICSEARCH_PROXY_ENDPOINT = `${host}/elasticsearch`;
+export const ELASTICSEARCH_PROXY_ENDPOINT = `${host}:${port}/elasticsearch`;
 export const ELASTICSEARCH_INDEX_NAME = "meadow";
