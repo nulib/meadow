91bc11a2f637c1d46af5a9c90b8347ffd7003086
Divya Katpally <katdivyareddy@gmail.com>
2020-04-22 08:35:31 -0500

    Design updates to search results

--- assets/js/components/UI/SearchBar.jsx
+++ assets/js/components/UI/SearchBar.jsx
@@ -4,29 +4,33 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const UISearchBar = () => {
   return (
-    <section className="section has-background-dark">
+    <section className="section has-background-white">
       <div className="container" data-testid="reactive-search-bar">
-        <DataSearch
-          componentId="SearchSensor"
-          autosuggest={true}
-          dataField={["accession_number"]}
-          debounce={100}
-          filterLabel="Work filter"
-          fuzziness={0}
-          highlight={true}
-          highlightField="title"
-          icon={<FontAwesomeIcon icon="search" />}
-          innerClass={{ input: "input is-medium" }}
-          queryFormat="or"
-          placeholder="Search Meadow"
-          react={{
-            and: ["SearchResult"]
-          }}
-          size={10}
-          showClear={true}
-          showFilter={true}
-          URLParams={true}
-        />
+        <div className="columns is-mobile">
+          <div className="column is-three-fifths is-offset-one-fifth">
+            <DataSearch
+              componentId="SearchSensor"
+              autosuggest={true}
+              dataField={["accession_number"]}
+              debounce={100}
+              filterLabel="Work filter"
+              fuzziness={0}
+              highlight={true}
+              highlightField="title"
+              icon={<FontAwesomeIcon icon="search" />}
+              innerClass={{ input: "input is-medium" }}
+              queryFormat="or"
+              placeholder="Search Meadow"
+              react={{
+                and: ["SearchResult"]
+              }}
+              size={10}
+              showClear={true}
+              showFilter={true}
+              URLParams={true}
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
