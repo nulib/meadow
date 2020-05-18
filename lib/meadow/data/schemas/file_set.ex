612db31a70b88e4fe713bfb93bcea0aee4beaa69
Karen Shaw Didrickson <karendid@gmail.com>
2020-05-18 11:34:25 -0500

    Index static public visiblity on file set

--- lib/meadow/data/schemas/file_set.ex
+++ lib/meadow/data/schemas/file_set.ex
@@ -57,6 +57,7 @@ defmodule Meadow.Data.Schemas.FileSet do
         description: file_set.metadata.description,
         create_date: file_set.inserted_at,
         modified_date: file_set.updated_at,
+        visibility: "open",
         work_id: file_set.work.id
       }
     end
