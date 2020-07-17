6e06fd40fbd67b1beb9f9cddf7d6e94377f662c5
katdivyareddy10 <14085957+katdivyareddy10@users.noreply.github.com>
2020-07-17 15:58:29 -0500

    Local cache setup for schemes in descriptive metadata (#955)
    * Local cache setup for schemes in descriptive metadata
    
    * Update the UI for displaying cached Code List values
    
    * useLazyQuery to load cached data
    
    * Avoid localStorage reset on first launch
    
    Co-authored-by: Adam Arling <adam.arling@northwestern.edu>

--- assets/js/components/Work/Tabs/About/DescriptiveMetadata.jsx
+++ assets/js/components/Work/Tabs/About/DescriptiveMetadata.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import UIFormField from "../../../UI/Form/Field";
 import UIFormFieldArray from "../../../UI/Form/FieldArray";
@@ -6,10 +6,19 @@ import UIControlledTermList from "../../../UI/ControlledTerm/List";
 import { CODE_LIST_QUERY } from "../../controlledVocabulary.gql.js";
 import UIFormFieldArrayDisplay from "../../../UI/Form/FieldArrayDisplay";
 import UIFormControlledTermArray from "../../../UI/Form/ControlledTermArray";
-import { useQuery } from "@apollo/react-hooks";
+import { useLazyQuery } from "@apollo/react-hooks";
 import UIError from "../../../UI/Error";
 import { DESCRIPTIVE_METADATA } from "../../../../services/metadata";
 import UISkeleton from "../../../UI/Skeleton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+const cacheNotification = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 const WorkTabsAboutDescriptiveMetadata = ({
   control,
@@ -19,21 +28,91 @@ const WorkTabsAboutDescriptiveMetadata = ({
   register,
   showDescriptiveMetadata,
 }) => {
-  const {
-    data: marcData,
-    loading: marcLoading,
-    errors: marcErrors,
-  } = useQuery(CODE_LIST_QUERY, { variables: { scheme: "MARC_RELATOR" } });
-  const {
-    data: subjectRoleData,
-    loading: subjectRoleLoading,
-    errors: subjectRoleErrors,
-  } = useQuery(CODE_LIST_QUERY, { variables: { scheme: "SUBJECT_ROLE" } });
-  const {
-    data: authorityData,
-    loading: authorityLoading,
-    errors: authorityErrors,
-  } = useQuery(CODE_LIST_QUERY, { variables: { scheme: "AUTHORITY" } });
+  const firstLaunch = useRef(true);
+  const [codeLists, setCodeLists] = useState(
+    localStorage.getItem("codeLists")
+      ? JSON.parse(localStorage.getItem("codeLists"))
+      : {}
+  );
+
+  useEffect(() => {
+    if (firstLaunch.current) {
+      firstLaunch.current = false;
+      return;
+    }
+    localStorage.setItem("codeLists", JSON.stringify(codeLists));
+  }, [codeLists]);
+
+  const [
+    getMarcData,
+    { data: marcData, loading: marcLoading, errors: marcErrors },
+  ] = useLazyQuery(CODE_LIST_QUERY, {
+    onCompleted: (data) => {
+      if (!marcErrors && data) {
+        setCodeLists({ ...codeLists, ["MARC_RELATOR"]: data.codeList });
+      }
+    },
+  });
+
+  const [
+    getSubjectRoleData,
+    {
+      data: subjectRoleData,
+      loading: subjectRoleLoading,
+      errors: subjectRoleErrors,
+    },
+  ] = useLazyQuery(CODE_LIST_QUERY, {
+    onCompleted: (data) => {
+      if (!subjectRoleErrors && data) {
+        setCodeLists({ ...codeLists, ["SUBJECT_ROLE"]: data.codeList });
+      }
+    },
+  });
+
+  const [
+    getAuthorityData,
+    { data: authorityData, loading: authorityLoading, errors: authorityErrors },
+  ] = useLazyQuery(CODE_LIST_QUERY, {
+    onCompleted: (data) => {
+      if (!authorityErrors && data) {
+        setCodeLists({ ...codeLists, ["AUTHORITY"]: data.codeList });
+      }
+    },
+  });
+
+  useEffect(() => {
+    if (!codeLists || !codeLists.MARC_RELATOR) {
+      getMarcData({
+        variables: { scheme: "MARC_RELATOR" },
+      });
+    }
+
+    if (!codeLists || !codeLists.SUBJECT_ROLE) {
+      getSubjectRoleData({
+        variables: { scheme: "SUBJECT_ROLE" },
+      });
+    }
+
+    if (!codeLists || !codeLists.AUTHORITY) {
+      getAuthorityData({
+        variables: { scheme: "AUTHORITY" },
+      });
+    }
+  }, []);
+
+  const refreshCache = () => {
+    setCodeLists({});
+    localStorage.clear();
+    getMarcData({
+      variables: { scheme: "MARC_RELATOR" },
+    });
+    getSubjectRoleData({
+      variables: { scheme: "SUBJECT_ROLE" },
+    });
+    getAuthorityData({
+      variables: { scheme: "AUTHORITY" },
+    });
+  };
 
   if (marcLoading || authorityLoading || subjectRoleLoading)
     return <UISkeleton rows={20} />;
@@ -41,33 +120,21 @@ const WorkTabsAboutDescriptiveMetadata = ({
     return (
       <UIError error={marcErrors || authorityErrors || subjectRoleErrors} />
     );
-  if (!authorityData || !marcData || !subjectRoleData) {
-    return (
-      <UIError
-        error={{ message: "No Authority, MARC, or Subject Role data" }}
-      />
-    );
-  }
 
   function getRoleDropDownOptions(scheme) {
     if (scheme === "MARC_RELATOR") {
-      return marcData.codeList;
+      return codeLists.MARC_RELATOR;
     }
     if (scheme === "SUBJECT_ROLE") {
-      return subjectRoleData.codeList;
+      return codeLists.SUBJECT_ROLE;
     }
     return [];
   }
 
-  // const codeLists = {
-  //   authorities: authorityData.codeList,
-  //   marcRelators: marcData.codeList,
-  //   subjectRole: subjectRoleData.codeList
-  // };
-
   return showDescriptiveMetadata ? (
     <div>
       <h3 className="subtitle is-size-5 is-marginless pb-4">Field Arrays</h3>
+
       <div className="columns is-multiline">
         {DESCRIPTIVE_METADATA.fieldArrays.map((item) => (
           <div key={item.name} className="column is-half">
@@ -92,13 +159,14 @@ const WorkTabsAboutDescriptiveMetadata = ({
 
       <hr />
       <h3 className="subtitle is-size-5 ">Controlled Terms</h3>
+
       <ul>
         {DESCRIPTIVE_METADATA.controlledTerms.map(({ label, name, scheme }) => (
           <li key={name} className="mb-5">
             <UIFormField label={label}>
               {isEditing ? (
                 <UIFormControlledTermArray
-                  authorities={authorityData.codeList}
+                  authorities={codeLists.AUTHORITY}
                   roleDropdownOptions={getRoleDropDownOptions(scheme)}
                   control={control}
                   errors={errors}
@@ -113,6 +181,24 @@ const WorkTabsAboutDescriptiveMetadata = ({
           </li>
         ))}
       </ul>
+      {isEditing && (
+        <div className="notification is-size-7" css={cacheNotification}>
+          <p>
+            <span className="icon">
+              <FontAwesomeIcon icon="bell" />
+            </span>
+            Role and Authority fields are using cached dropdown values (as these
+            rarely change).
+          </p>
+          <button
+            type="button"
+            className="button is-text is-small"
+            onClick={refreshCache}
+          >
+            Sync with latest values
+          </button>
+        </div>
+      )}
     </div>
   ) : null;
 };
