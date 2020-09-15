e71743dff0c81d959af98417b88e76c5fa4765fa
Michael B. Klein <mbklein@gmail.com>
2020-09-15 13:13:49 -0500

    Refactor Application children; don't start mock ARK server if the port is in use

---
+++ lib/meadow/application/children.ex
@@ -0,0 +1,54 @@
+defmodule Meadow.Application.Children do
+  @moduledoc """
+  Child specs for Meadow.Application
+  """
+  alias Meadow.Application.Caches
+  alias Meadow.Config
+  alias Meadow.Utils.ArkClient
+  require Logger
+
+  def specs do
+    (Caches.specs(Config.environment()) ++
+       specs(Config.environment()))
+    |> Enum.reject(&is_nil/1)
+  end
+
+  defp specs(:dev) do
+    [
+      {Meadow.Data.IndexWorker, interval: Config.index_interval()},
+      Meadow.IIIF.ManifestListener,
+      mock_ark_server(3943)
+    ]
+  end
+
+  defp specs(:test) do
+    [mock_ark_server(3944)]
+  end
+
+  defp specs(:prod) do
+    [
+      {Meadow.Data.IndexWorker, interval: Config.index_interval()},
+      Meadow.IIIF.ManifestListener
+    ]
+  end
+
+  defp mock_ark_server(port) do
+    plug = ArkClient.MockServer
+
+    case :gen_tcp.connect('localhost', port, [], 50) do
+      {:ok, _} ->
+        "Skipping launch of #{inspect(plug)}. Port #{port} already in use."
+        |> Logger.info()
+
+        nil
+
+      {:error, _} ->
+        cowboy_version = Application.spec(:cowboy)[:vsn]
+
+        "Running #{inspect(plug)} with cowboy #{cowboy_version} at 0.0.0.0:#{port} (http)"
+        |> Logger.info()
+
+        {Plug.Cowboy, scheme: :http, plug: plug, options: [port: port]}
+    end
+  end
+end
