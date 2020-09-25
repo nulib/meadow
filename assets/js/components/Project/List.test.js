26eab51529842afe27adf3b4931b5c4709c5361e
Divya Katpally <katdivyareddy@gmail.com>
2020-09-25 08:58:12 -0500

    Filter projects by title

--- assets/js/components/Project/List.test.js
+++ assets/js/components/Project/List.test.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ProjectList from "./List";
 import { renderWithRouterApollo } from "../../services/testing-helpers";
-import { fireEvent } from "@testing-library/react";
+import { fireEvent, act } from "@testing-library/react";
 import { Route } from "react-router-dom";
-import { getProjectsMock } from "./project.gql.mock";
+import {
+  getProjectsMock,
+  MOCK_PROJECT_ID,
+  MOCK_PROJECT_ID_2,
+} from "./project.gql.mock";
 
 const mocks = [getProjectsMock];
 
@@ -27,9 +31,39 @@ it("renders the ProjectsList component", async () => {
   expect(projectListEl).toBeInTheDocument();
 });
 
+it("renders list of projects", async () => {
+  const { findByTestId, debug } = setupMatchTests();
+  const projectEl1 = await findByTestId(`project-title-${MOCK_PROJECT_ID}`);
+  const projectEl2 = await findByTestId(`project-title-${MOCK_PROJECT_ID_2}`);
+  expect(projectEl1).toBeInTheDocument();
+  expect(projectEl2).toBeInTheDocument();
+});
+
+it("filters for a project by title", async () => {
+  const { findByTestId, debug } = setupMatchTests();
+  const el = await findByTestId("input-project-filter");
+  expect(el).toBeInTheDocument();
+
+  const projectEl1 = await findByTestId(`project-title-${MOCK_PROJECT_ID}`);
+  const projectEl2 = await findByTestId(`project-title-${MOCK_PROJECT_ID_2}`);
+  expect(projectEl1).toBeInTheDocument();
+  expect(projectEl2).toBeInTheDocument();
+
+  await act(async () => {
+    fireEvent.change(el, { target: { value: "Second" } });
+  });
+  expect(el.value).toEqual("Second");
+
+  expect(projectEl1).not.toBeInTheDocument();
+  expect(projectEl2).toBeInTheDocument();
+});
+
 it("opens delete modal", async () => {
   const { findByTestId, debug } = setupMatchTests();
-  const deleteButtonEl = await findByTestId("delete-button");
+  const deleteButtonEl = await findByTestId(
+    "delete-button-01DNFK4B8XASXNKBSAKQ6YVNF3"
+  );
+  expect(deleteButtonEl).toBeInTheDocument();
   fireEvent.click(deleteButtonEl);
   const deleteModalEl = await findByTestId("delete-modal");
   expect(deleteModalEl).toBeInTheDocument();
