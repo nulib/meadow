c745d2b57591071e8072e5cdd89aa70544435f57
Divya Katpally <katdivyareddy@gmail.com>
2020-10-05 10:11:01 -0500

    Add button to delete items from BatchEdit Remove

--- assets/js/components/BatchEdit/Remove.js
+++ assets/js/components/BatchEdit/Remove.js
@@ -3,6 +3,7 @@ import UIFormField from "../UI/Form/Field";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PropTypes from "prop-types";
 import { splitFacetKey } from "../../services/metadata";
+import { useBatchDispatch } from "../../context/batch-edit-context";
 
 export default function BatchEditRemove({
   handleRemoveClick,
@@ -10,6 +11,16 @@ export default function BatchEditRemove({
   name,
   removeItems,
 }) {
+  const dispatch = useBatchDispatch();
+
+  const removeFromDelete = (e, item) => {
+    e.preventDefault();
+    dispatch({
+      type: "updateRemoveItem",
+      fieldName: name,
+      key: item,
+    });
+  };
   return (
     <div data-testid="batch-edit-remove">
       <UIFormField>
@@ -31,8 +42,18 @@ export default function BatchEditRemove({
           <ul className="py-4">
             {removeItems.map((item) => {
               const { label, role, term } = splitFacetKey(item);
-
-              return <li key={item}>{`${label} - ${term}`}</li>;
+              return (
+                <li key={item}>
+                  {`${label} - ${term}`}
+                  <span
+                    className="is-pulled-right icon mr-2"
+                    onClick={(e) => removeFromDelete(e, item)}
+                    data-testid="remove-delete-entries"
+                  >
+                    {<FontAwesomeIcon icon="trash" />}
+                  </span>
+                </li>
+              );
             })}
           </ul>
         </div>
