363f59119beb7f22f452fa3745ecbe9c63be2df8
Adam Arling <adam.arling@northwestern.edu>
2020-10-09 11:02:44 -0500

    Quick UIWorkImage component code compress

--- assets/js/components/UI/WorkImage.jsx
+++ assets/js/components/UI/WorkImage.jsx
@@ -1,22 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function UIWorkImage({ imageUrl, size = 128 }) {
-  if (!imageUrl) {
-    return (
-      <img
-        data-testid="image-source"
-        src={`https://bulma.io/images/placeholders/${size}x${size}.png`}
-      />
-    );
-  }
+function UIWorkImage({ imageUrl = "", size = 128 }) {
+  const src = imageUrl
+    ? `${imageUrl}/square/${size},${size}/0/default.jpg`
+    : `https://bulma.io/images/placeholders/${size}x${size}.png`;
 
-  return (
-    <img
-      data-testid="image-source"
-      src={`${imageUrl}/square/${size},${size}/0/default.jpg`}
-    />
-  );
+  return <img data-testid="image-source" src={src} />;
 }
 
 UIWorkImage.propTypes = {
