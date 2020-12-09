3b5bf1bbdc7d89748f58f5d5203ef8582f8bb3cf
Adam Arling <adam.arling@northwestern.edu>
2020-12-09 15:00:56 -0600

    PR feedback updates

--- assets/js/components/UI/Date.jsx
+++ assets/js/components/UI/Date.jsx
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import moment from "moment";
 
 function UIDate({ dateString }) {
+  if (!dateString) return null;
+
   return (
     <React.Fragment>
       {moment(dateString).format("MMM DD, YYYY h:mm A")}
--- assets/js/components/UI/Layout/Footer.jsx
+++ assets/js/components/UI/Layout/Footer.jsx
@@ -4,43 +4,33 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const UILayoutFooter = () => {
   return (
     <footer className="footer has-background-white has-text-grey">
-      <div className="container">
-        <nav className="level">
-          <div className="level-item">
-            <a
-              className="navbar-item"
-              href="https://www.library.northwestern.edu/"
-            >
-              <figure className="image logo">
-                <img
-                  src="/images/northwestern-libraries-logo.png"
-                  alt="Northwestern Libraries logo"
-                />
-              </figure>
-            </a>
-          </div>
-          <div className="level-item">
-            <div>
-              <span className="is-family-secondary">Meadow v1.x.x</span> by{" "}
-              <a href="https://github.com/nulib" target="_blank">
-                @nulib
+      <div className="container has-text-centered">
+        <div className="column is-half is-inline-block">
+          <nav className="level">
+            <div className="level-item">
+              <a
+                className="navbar-item"
+                href="https://www.library.northwestern.edu/"
+              >
+                <figure className="image logo">
+                  <img
+                    src="/images/northwestern-libraries-logo.png"
+                    alt="Northwestern Libraries logo"
+                    style={{ opacity: 0.5 }}
+                  />
+                </figure>
               </a>
             </div>
-          </div>
-          <div className="level-item">
-            <p>
-              An{" "}
-              <a href="https://elixir-lang.org/" target="_blank">
-                Elixir
-              </a>
-              /
-              <a href="https://reactjs.org/" target="_blank">
-                React
-              </a>{" "}
-              repository application
-            </p>
-          </div>
-        </nav>
+            <div className="level-item">
+              <div>
+                <span className="is-family-secondary">Meadow v1.x.x</span> by{" "}
+                <a href="https://github.com/nulib" target="_blank">
+                  @nulib
+                </a>
+              </div>
+            </div>
+          </nav>
+        </div>
       </div>
     </footer>
   );
