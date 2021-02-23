54d0cf817b4a0cd9a0fc3bda917d7660bf48ff60
Michael B. Klein <mbklein@gmail.com>
2021-02-23 14:52:44 -0600

    Create meadow upload user with access keys

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,10 @@ output "ec2_instance" {
 output "endpoint" {
   value = "https://${aws_route53_record.app_hostname.fqdn}/"
 }
+
+output "upload_user_key_id" {
+  value = {
+    aws_access_key_id = aws_iam_access_key.upload_user_access_key.id,
+    aws_secret_access_key = aws_iam_access_key.upload_user_access_key.secret
+  }
+}
