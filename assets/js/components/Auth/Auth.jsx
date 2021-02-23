16cce7029a1aae5de002ccdfba9542124d62d3f3
Karen Shaw <karendid@gmail.com>
2021-02-23 12:22:33 -0700

    Configure Honeybadger with front end

--- assets/js/components/Auth/Auth.jsx
+++ assets/js/components/Auth/Auth.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import Error from "../UI/Error";
 import { GET_CURRENT_USER_QUERY } from "./auth.gql";
+import Honeybadger from "@honeybadger-io/js";
 
 export const AuthContext = React.createContext();
 
@@ -10,6 +11,13 @@ export const AuthProvider = ({ children }) => {
 
   if (error) return <Error error={error} />;
   if (loading) return null;
+
+  Honeybadger.setContext({
+    user_id: data.me.username,
+    user_email: data.me.email,
+    user_display_name: data.me.displayName,
+    user_role: data.me.role,
+  });
 
   return (
     <AuthContext.Provider value={data.me}>{children}</AuthContext.Provider>
