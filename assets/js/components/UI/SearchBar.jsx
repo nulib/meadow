b5161a4ecda830c00b159923007e3f7637029872
Adam J. Arling <adam.arling@northwestern.edu>
2021-03-11 11:46:49 -0600

    Update Elasticsearch search fields to include Collection title

--- assets/js/components/UI/SearchBar.jsx
+++ assets/js/components/UI/SearchBar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { DataSearch } from "@appbaseio/reactivesearch";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ELASTICSEARCH_FIELDS_TO_SEARCH } from "../../services/elasticsearch";
 import { RESULT_SENSOR, SEARCH_SENSOR } from "../../services/reactive-search";
 import userPreviousQueryParts from "@js/hooks/usePreviousQueryParts";
@@ -29,11 +28,11 @@ const UISearchBar = () => {
         dataField={ELASTICSEARCH_FIELDS_TO_SEARCH}
         debounce={500}
         defaultValue={prepDefaultValue()}
-        fieldWeights={[5, 2]}
+        fieldWeights={[5, 2, 2]} // These weights correspond to the index positions of ELASTICSEARCH_FIELDS_TO_SEARCH
         filterLabel="Search"
         fuzziness="AUTO"
         icon={<IconSearch />}
-        innerClass={{ input: "input is-large" }}
+        innerClass={{ input: "input is-large rs-input" }}
         queryFormat="or"
         queryString={true} // supports complex search, wildcards, etc.
         placeholder="Search all works"
