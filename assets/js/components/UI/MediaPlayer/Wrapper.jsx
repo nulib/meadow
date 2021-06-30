cf79ecf6f6bb4956bd775e77e6ee4bc6eb26bfbd
Adam J. Arling <aarling@gmail.com>
2021-06-30 11:48:04 -0500

    Handle error messages when adding a new Media file set

--- assets/js/components/UI/MediaPlayer/Wrapper.jsx
+++ assets/js/components/UI/MediaPlayer/Wrapper.jsx
@@ -3,6 +3,9 @@ import PropTypes from "prop-types";
 import MediaPlayer from "@js/components/UI/MediaPlayer/MediaPlayer";
 import useTechnicalMetadata from "@js/hooks/useTechnicalMetadata";
 import useFileSet from "@js/hooks/useFileSet";
+import { Notification } from "@nulib/admin-react-components";
+import UIIconText from "@js/components/UI/IconText";
+import { IconAlert } from "@js/components/Icon";
 
 const vttSampleUrl =
   "https://s3.amazonaws.com/demo.jwplayer.com/text-tracks/assets/chapters.vtt";
@@ -22,22 +25,42 @@ function MediaPlayerWrapper({ fileSet }) {
   const { getTechnicalMetadata } = useTechnicalMetadata();
   const mediaInfoTracks = getTechnicalMetadata(fileSet);
   const { isEmpty } = useFileSet();
+
   if (isEmpty(fileSet)) return null;
+
+  {
+    /* FileSet hasn't yet been fully ran through the pipeline */
+  }
+  if (!fileSet?.coreMetadata?.mimeType) {
+    return (
+      <Notification isWarning>
+        <UIIconText isCentered icon={<IconAlert />}>
+          The media file is being processed. Click refresh your browser in a few
+          seconds
+        </UIIconText>
+      </Notification>
+    );
+  }
 
   const sources = [
     {
-      id: fileSet?.streamingUrl,
-      type: mediaInfoTracks[1]["@type"],
-      format: fileSet?.coreMetadata?.mimeType,
-      height: mediaInfoTracks[1].Height,
-      width: mediaInfoTracks[1].Width,
-      duration: mediaInfoTracks[1].Duration,
+      src: fileSet?.streamingUrl,
+      type: fileSet?.coreMetadata?.mimeType,
     },
   ];
+  const videoElAttrs = mediaInfoTracks &&
+    mediaInfoTracks[1] && {
+      height: mediaInfoTracks[1].Height,
+      width: mediaInfoTracks[1].Width,
+    };
 
   return (
     <div>
-      <MediaPlayer sources={sources} tracks={mockVideoTracks} />
+      <MediaPlayer
+        sources={sources}
+        tracks={mockVideoTracks}
+        videoElAttrs={videoElAttrs}
+      />
     </div>
   );
 }
