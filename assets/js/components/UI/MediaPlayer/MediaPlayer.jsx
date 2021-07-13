4bf4eeea5820795c51f9e4c4ed01aff6c2dbcfa2
Adam J. Arling <aarling@gmail.com>
2021-07-13 15:47:25 -0500

    Wiring up might look something like this?

--- assets/js/components/UI/MediaPlayer/MediaPlayer.jsx
+++ assets/js/components/UI/MediaPlayer/MediaPlayer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import MediaPlayerNav from "@js/components/UI/MediaPlayer/Nav";
+import Hls from "hls.js";
 
 function MediaPlayer({
   navCues = [],
@@ -10,6 +11,34 @@ function MediaPlayer({
   ...restProps
 }) {
   const playerRef = React.useRef();
+
+  React.useEffect(() => {
+    if (sources.length === 0) return;
+
+    console.log(`Hls.isSupported()`, Hls.isSupported());
+
+    const video = playerRef.current;
+    const url =
+      "https://meadow-streaming.rdc-staging.library.northwestern.edu/85/bd/1f/cd/-5/ff/6-/45/fb/-a/c5/1-/e4/56/44/6d/cb/00/6298d09f04833eb737504941812b0442e6253a4e286e79db3b11e16f9b39c604.m3u8";
+    const hls = new Hls();
+
+    hls.attachMedia(playerRef.current);
+    hls.on(Hls.Events.MEDIA_ATTACHED, function () {
+      console.log("video and hls.js are now bound together !");
+      hls.loadSource(url);
+      hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
+        console.log(
+          "manifest loaded, found " + data.levels.length + " quality level"
+        );
+      });
+    });
+    hls.on(Hls.Events.ERROR, function (event, data) {
+      console.log(`data`, data);
+      var errorType = data.type;
+      var errorDetails = data.details;
+      var errorFatal = data.fatal;
+    });
+  }, [sources]);
 
   const handleNavClick = (e, cue) => {
     e.preventDefault();
@@ -28,14 +57,25 @@ function MediaPlayer({
           {...videoElAttrs}
           {...restProps}
         >
-          {sources.map(({ src, type }) => (
+          {/* {sources.map(({ src, type }) => (
             <source key={src} data-testid="source-item" src={src} type={type} />
-          ))}
+          ))} */}
+          {/* <source
+            src={`https://meadow-streaming.rdc-staging.library.northwestern.edu/85/bd/1f/cd/-5/ff/6-/45/fb/-a/c5/1-/e4/56/44/6d/cb/00/6298d09f04833eb737504941812b0442e6253a4e286e79db3b11e16f9b39c604.m3u8`}
+          /> */}
         </video>
         <div className="block" className="column is-one-quarter">
           <h3>Video nav</h3>
           <MediaPlayerNav cues={navCues} handleNavClick={handleNavClick} />
         </div>
+      </div>
+      <div>
+        <video
+          width="600"
+          controls
+          height="400"
+          src="https://meadow-streaming.rdc-staging.library.northwestern.edu/85/bd/1f/cd/-5/ff/6-/45/fb/-a/c5/1-/e4/56/44/6d/cb/00/6298d09f04833eb737504941812b0442e6253a4e286e79db3b11e16f9b39c604.m3u8"
+        />
       </div>
     </>
   );
