619afefde4a0f3be30dc5a5d98d9d07c6fe826f7
Michael B. Klein <mbklein@gmail.com>
2021-08-16 14:54:22 -0500

    Build and push Docker image

---
+++ .github/scripts/upload_source_maps.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+container_id=$(docker create nulib/meadow:${DOCKER_TAG})
+docker cp ${container_id}:/app/lib/meadow-${MEADOW_VERSION}/priv/static/js ${container_id}
+for source in app vendors~app; do
+  js_file=$(ls ${container_id}/${source}.bundle-*.js)
+  source_map=$(ls ${container_id}/${source}.js-*.map)
+  curl https://api.honeybadger.io/v1/source_maps \
+      -F api_key=${HONEYBADGER_API_KEY_FRONTEND} \
+      -F revision=${HONEYBADGER_REVISION} \
+      -F minified_url="https://meadow*.library.northwestern.edu/js/$(basename ${js_file})" \
+      -F source_map=@${source_map} \
+      -F minified_file=@${js_file}
+done
