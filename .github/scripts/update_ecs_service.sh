d331d23c696e7bf5441a412126c7bbfe2060b8f5
Michael B. Klein <mbklein@gmail.com>
2021-09-09 10:50:44 -0500

    Use github event refs to look for index changes instead of HEAD and HEAD^

--- .github/scripts/update_ecs_service.sh
+++ .github/scripts/update_ecs_service.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-changed_files=$(git diff --name-only HEAD HEAD^)
+echo "Looking for index changes between ${NEW_HEAD} and ${PRIOR_HEAD}"
+changed_files=$(git diff --name-only ${NEW_HEAD} ${PRIOR_HEAD})
 case $changed_files in
   lib/meadow/indexing/*)
     reindex=true
@@ -12,6 +13,7 @@ case $changed_files in
     reindex=false
     ;;
 esac
+echo "Reindex: ${reindex}"
 
 networkconfig=$(aws ecs describe-services --cluster ${ECS_CLUSTER} --service ${ECS_SERVICE} | jq -cM '.services[0].networkConfiguration')
 overrides='{"containerOverrides":[{"name":"'${ECS_CONTAINER}'","environment": [{"name": "MEADOW_PROCESSES", "value": "none"}, {"name": "DB_POOL_SIZE", "value": "10"}],"command":["eval","Meadow.ReleaseTasks.migrate('${reindex}')"]}]}'
