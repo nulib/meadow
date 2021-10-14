6c0ce3f94dddb5694fd664b61729089ce545ff27
Karen Shaw <karendid@gmail.com>
2021-10-14 13:00:47 -0600

    Updates to iiif manifest generation

--- lib/meadow/data/schemas/iiif/V3/work.ex
+++ lib/meadow/data/schemas/iiif/V3/work.ex
@@ -35,6 +35,7 @@ defimpl Meadow.IIIF.V3.Resource, for: Meadow.Data.Schemas.Work do
       %Canvas{
         id: IIIF.V3.canvas_id(work.id, file_set.id),
         label: %Label{en: [file_set.core_metadata.label]},
+        duration: duration_in_seconds(FileSets.duration_in_milliseconds(file_set)),
         thumbnail:
           case FileSets.representative_image_url_for(file_set) do
             nil ->
@@ -61,9 +62,10 @@ defimpl Meadow.IIIF.V3.Resource, for: Meadow.Data.Schemas.Work do
         items: [
           %AnnotationPage{
             id: IIIF.V3.annotation_page_id(work.id, file_set.id, "1"),
-            items: access_file_annotations(work, file_set)
+            items: access_file_media_annotations(work, file_set)
           }
-        ]
+        ],
+        annotations: access_file_vtt_annotation_page(work, file_set)
       }
     end)
   end
@@ -104,48 +106,51 @@ defimpl Meadow.IIIF.V3.Resource, for: Meadow.Data.Schemas.Work do
     end)
   end
 
-  defp access_file_annotations(
+  defp access_file_media_annotations(work, file_set) do
+    [
+      %Annotation{
+        id: IIIF.V3.annotation_id(work.id, file_set.id, "1", "1"),
+        body: %Content{
+          id: FileSets.distribution_streaming_uri_for(file_set),
+          type: String.capitalize(resource_type(work.work_type.id)),
+          format: file_set.core_metadata.mime_type,
+          height: FileSets.height(file_set),
+          width: FileSets.width(file_set),
+          duration: duration_in_seconds(FileSets.duration_in_milliseconds(file_set))
+        },
+        target: IIIF.V3.canvas_id(work.id, file_set.id)
+      }
+    ]
+  end
+
+  defp access_file_vtt_annotation_page(
          work,
          %{structural_metadata: %{type: "webvtt", value: _value}} = file_set
        ) do
-    [access_file_media_annotation(work, file_set) | [access_file_vtt_annotation(work, file_set)]]
+    [
+      %AnnotationPage{
+        id: IIIF.V3.annotation_page_id(work.id, file_set.id, "a1"),
+        items: [
+          %Annotation{
+            id: IIIF.V3.annotation_id(work.id, file_set.id, "1", "2"),
+            motivation: "supplementing",
+            body: %Content{
+              id: FileSets.public_vtt_url_for(file_set.id),
+              type: "Text",
+              format: "text/vtt",
+              label: %Label{
+                en: ["Transcript"]
+              },
+              language: "en"
+            },
+            target: IIIF.V3.canvas_id(work.id, file_set.id)
+          }
+        ]
+      }
+    ]
   end
 
-  defp access_file_annotations(work, file_set) do
-    [access_file_media_annotation(work, file_set)]
-  end
-
-  defp access_file_media_annotation(work, file_set) do
-    %Annotation{
-      id: IIIF.V3.annotation_id(work.id, file_set.id, "1", "1"),
-      body: %Content{
-        id: FileSets.distribution_streaming_uri_for(file_set),
-        type: String.capitalize(resource_type(work.work_type.id)),
-        format: file_set.core_metadata.mime_type,
-        height: FileSets.height(file_set),
-        width: FileSets.width(file_set),
-        duration: duration_in_seconds(FileSets.duration_in_milliseconds(file_set))
-      },
-      target: IIIF.V3.canvas_id(work.id, file_set.id)
-    }
-  end
-
-  defp access_file_vtt_annotation(work, file_set) do
-    %Annotation{
-      id: IIIF.V3.annotation_id(work.id, file_set.id, "1", "2"),
-      motivation: "supplementing",
-      body: %Content{
-        id: FileSets.public_vtt_url_for(file_set.id),
-        type: "Text",
-        format: "text/vtt",
-        label: %Label{
-          en: ["Transcript"]
-        },
-        language: "en"
-      },
-      target: IIIF.V3.canvas_id(work.id, file_set.id)
-    }
-  end
+  defp access_file_vtt_annotation_page(_work, _file_set), do: []
 
   defp duration_in_seconds(nil), do: nil
   defp duration_in_seconds(duration) when duration > 0, do: duration / 1000
