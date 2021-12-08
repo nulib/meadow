6d6c415ce6282b2c9192d0c54bf75b0a1dd14d0b
Michael B. Klein <mbklein@gmail.com>
2021-12-08 12:14:43 -0600

    Add AVR subject import

--- priv/avr_migration/README.md
+++ priv/avr_migration/README.md
@@ -1,22 +1,30 @@
 # AVR Migration Procedure
 
 1. Download/sync the migration XML and CSV files from `s3://stack-p-avr-masterfiles/meadow-migration/`
-    to a local directory.
+   to a local directory.
 2. Open a Meadow IEx console with `MEADOW_PROCESSES=none`.
-3. Load the migration module.
+3. Create an AVR Migration project in Meadow.
+4. Upload the AVR MODS XML files to the AVR Migration project S3 folder and wait a few
+   minutes for all the digest tags to populate. **Note:** If running this in the dev environment,
+   try to upload things in smaller chunks, like one subdirectory at a time with a few seconds'
+   pause in between. The `minio-checksum` container has trouble keeping up under heavy load.
+5. Ingest `avr_migration.csv` into the AVR Migration project. It should take about 2 minutes to 
+   validate and 20 to ingest.
+6. Copy the ID of the completed ingest sheet.
+7. Load the migration module.
    ```elixir
    iex> Code.compile_file("priv/avr_migration/avr_migration.exs")
    ```
-4. Create AVR collections.
+8. Create AVR collections.
    ```elixir
    iex> AVR.Migration.import_collections()
    ```
-5. Create an AVR Migration project in Meadow.
-6. Upload the AVR MODS XML files to the AVR Migration project S3 folder and wait a few
-    minutes for all the digest tags to populate.
-7. Ingest `avr_migration.csv` into the AVR Migration project. It should take about 2 minutes to validate and 20 to ingest.
-8. Copy the ID of the completed ingest sheet.
 9. Link ingested works to their collections.
    ```elixir
    iex> AVR.Migration.map_works_to_collections(ingest_sheet_id)
    ```
+10. Create AVR Subject Headings.
+    ```elixir
+    iex> AVR.Migration.import_subjects()
+    ```
+11. Do a CSV Metadata Spreadsheet Export of the completed ingest sheet's works.
--- priv/avr_migration/avr_migration.exs
+++ priv/avr_migration/avr_migration.exs
@@ -4,8 +4,10 @@ defmodule AVR.Migration do
   alias Meadow.Ingest.Rows
   alias Meadow.Ingest.Schemas.Row
   alias Meadow.Repo
+  alias NUL.Schemas.AuthorityRecord
+  alias NUL.AuthorityRecords
 
-  import Ecto.Query
+  import Ecto.{Changeset, Query}
 
   def import_collections do
     collections()
@@ -15,6 +17,54 @@ defmodule AVR.Migration do
         collection -> {:ok, collection}
       end
     end)
+  end
+
+  def import_subjects(write_back \\ false) do
+    with subjects <- subjects() |> Enum.map(&find_or_create_subject/1) do
+      if write_back, do: save_json(subjects, "avr_subjects.json")
+    end
+  end
+
+  def import_filesets(bucket, prefix) do
+    ExAws.S3.list_objects_v2(bucket, prefix: prefix)
+    |> ExAws.stream!()
+    |> Stream.each(fn %{key: key} ->
+      ExAws.S3.get_object(bucket, key)
+      |> ExAws.request!()
+      |> Map.get(:body)
+      |> Jason.decode!(keys: :atoms)
+      |> import_work_filesets()
+    end)
+    |> Stream.run()
+  end
+
+
+  defp find_or_create_subject(%{id: id} = attributes) do
+    case AuthorityRecords.get_authority_record(id) do
+      nil ->
+        with {:ok, result} <-
+               %AuthorityRecord{}
+               |> AuthorityRecord.changeset(attributes)
+               |> put_change(:id, id)
+               |> Repo.insert() do
+          result
+        end
+
+      record ->
+        record
+    end
+  end
+
+  defp find_or_create_subject(attributes) do
+    case from(r in AuthorityRecord, where: r.label == ^attributes.label) |> Repo.all() do
+      [record | _] ->
+        record
+
+      [] ->
+        with {:ok, result} <- AuthorityRecords.create_authority_record(attributes) do
+          result
+        end
+    end
   end
 
   def map_works_to_collections(ingest_sheet_id) do
@@ -53,19 +103,47 @@ defmodule AVR.Migration do
     |> Enum.into(%{})
   end
 
-  defp collections(binary_ids \\ true) do
+  def collections(binary_ids \\ true), do: load_json("avr_collections.json", binary_ids)
+  def subjects, do: load_json("avr_subjects.json", false)
+
+  defp load_json(file, binary_ids) do
     result =
-      Meadow.Config.priv_path("avr_migration/avr_collections.json")
+      Meadow.Config.priv_path("avr_migration/#{file}")
       |> File.read!()
       |> Jason.decode!(keys: :atoms)
 
     if binary_ids do
       result
-      |> Enum.map(fn attrs ->
-        Map.update!(attrs, :id, &Ecto.UUID.dump!/1)
+      |> Enum.map(fn
+        %{id: _} = attrs -> Map.update!(attrs, :id, &Ecto.UUID.dump!/1)
+        attrs -> attrs
       end)
     else
       result
     end
+  end
+
+  defp save_json(content, file) do
+    content =
+      content
+      |> Enum.map(fn record ->
+        record
+        |> Map.from_struct()
+        |> Enum.reject(fn {key, _} -> Enum.member?([:__meta__, :inserted_at, :updated_at], key) end)
+        |> Enum.into(%{})
+      end)
+      |> Enum.map(fn
+        %{id: id} = record ->
+          case Ecto.UUID.cast(id) do
+            {:ok, result} -> Map.put(record, :id, result)
+            _ -> record
+          end
+
+        record ->
+          record
+      end)
+
+    Meadow.Config.priv_path("avr_migration/#{file}")
+    |> File.write!(Jason.encode!(content))
   end
 end
