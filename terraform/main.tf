c85b398fdb9835b3e78b4d020272ff8ede3e40d7
Brendan Quinn <brendan-quinn@northwestern.edu>
2022-02-04 09:30:12 -0600

    Terraform preservation bucket replication for production environment only

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,12 @@ terraform {
   backend "s3" {
     key = "meadow.tfstate"
   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
 
 provider "aws" {
@@ -82,6 +88,12 @@ resource "aws_s3_bucket" "meadow_preservation" {
 
   versioning {
     enabled = true
+  }
+
+  lifecycle {
+    ignore_changes = [
+      replication_configuration
+    ]
   }
 
   lifecycle_rule {
@@ -520,4 +532,89 @@ resource "aws_route53_record" "meadow_streaming_cloudfront" {
   type    = "CNAME"
   ttl     = "900"
   records = [aws_cloudfront_distribution.meadow_streaming.domain_name]
+}
+
+provider "aws" {
+  alias  = "west"
+  region = "us-west-1"
+}
+
+resource "aws_iam_role" "replication_role" {
+  # Only create the role in production env
+  count = var.environment == "p" ? 1 : 0
+  name  = "${var.stack_name}-replication-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "s3.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "${var.stack_name}-replication-policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect   = "Allow"
+          Action   = ["s3:GetReplicationConfiguration", "s3:ListBucket"]
+          Resource = ["${aws_s3_bucket.meadow_preservation.arn}"]
+        },
+        {
+          Effect   = "Allow"
+          Action   = ["s3:GetObjectVersionForReplication", "s3:GetObjectVersionAcl", "s3:GetObjectVersionTagging"]
+          Resource = ["${aws_s3_bucket.meadow_preservation.arn}/*"]
+        },
+        {
+          Effect   = "Allow"
+          Action   = ["s3:ReplicateObject", "s3:ReplicateDelete", "s3:ReplicateTags"]
+          Resource = ["${aws_s3_bucket.meadow_preservation_replica[count.index].arn}/*"]
+        }
+      ]
+    })
+  }
+}
+
+resource "aws_s3_bucket" "meadow_preservation_replica" {
+  count    = var.environment == "p" ? 1 : 0
+  provider = aws.west
+  bucket   = "${var.stack_name}-${var.environment}-preservation-replica"
+
+  versioning {
+    enabled = true
+  }
+
+  tags = var.tags
+}
+
+resource "aws_s3_bucket_replication_configuration" "east_to_west" {
+  count  = var.environment == "p" ? 1 : 0
+  role   = aws_iam_role.replication_role[count.index].arn
+  bucket = aws_s3_bucket.meadow_preservation.id
+
+  rule {
+    id     = "preservation-replica"
+    status = "Enabled"
+
+    filter {
+      prefix = ""
+    }
+
+    delete_marker_replication {
+      status = "Enabled"
+    }
+
+    destination {
+      bucket        = aws_s3_bucket.meadow_preservation_replica[count.index].arn
+      storage_class = "DEEP_ARCHIVE"
+    }
+  }
 }
