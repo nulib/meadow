db59872d0261b721a16a7bb14c59f939f698295e
Karen Shaw <karendid@gmail.com>
2022-10-13 12:32:37 +0000

    Send bucket size for agg terms in batch edit query

--- app/assets/js/services/elasticsearch.js
+++ app/assets/js/services/elasticsearch.js
@@ -16,41 +16,49 @@ export const ELASTICSEARCH_AGGREGATION_FIELDS = {
   contributor: {
     terms: {
       field: "descriptiveMetadata.contributor.facet",
+      size: 1000,
     },
   },
   creator: {
     terms: {
       field: "descriptiveMetadata.creator.facet",
+      size: 1000,
     },
   },
   genre: {
     terms: {
       field: "descriptiveMetadata.genre.facet",
+      size: 1000,
     },
   },
   language: {
     terms: {
       field: "descriptiveMetadata.language.facet",
+      size: 1000,
     },
   },
   location: {
     terms: {
       field: "descriptiveMetadata.location.facet",
+      size: 1000,
     },
   },
   stylePeriod: {
     terms: {
       field: "descriptiveMetadata.stylePeriod.facet",
+      size: 1000,
     },
   },
   subject: {
     terms: {
       field: "descriptiveMetadata.subject.facet",
+      size: 1000,
     },
   },
   technique: {
     terms: {
       field: "descriptiveMetadata.technique.facet",
+      size: 1000,
     },
   },
 };
