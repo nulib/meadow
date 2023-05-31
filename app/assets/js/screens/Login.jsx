7d084e5f44e53cf485e9ee348ef60499d2aef0a1
Mat Jordan <mat@northwestern.edu>
2023-05-31 13:05:52 +0000

    Update dcapi-types, add login statement.

--- app/assets/js/screens/Login.jsx
+++ app/assets/js/screens/Login.jsx
@@ -30,6 +30,19 @@ const ScreensLogin = () => {
               Meadow.
             </UIIconText>
           </Notification>
+          <p className="is-size-6 has-text-left p-5">
+            Northwestern University Libraries and its digitized collections in
+            Meadow contain materials that reflect the beliefs and norms of the
+            era and culture in which they were created or collected. As such,
+            staff who log into Meadow may encounter imagery, language, or
+            opinions related to a white supremacist, exploitative, and/or
+            discriminatory culture that they find offensive. Additionally, some
+            collections may contain sexual content or violence that may not be
+            appropriate for all audiences. The Libraries metadata staff are
+            actively working to remediate issues of outdated, inaccurate,
+            objectifying or disrespectful language that describes our
+            collections and welcome suggestions, questions, or comments.
+          </p>
         </div>
       </div>
     </Layout>
