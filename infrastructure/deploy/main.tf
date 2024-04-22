56a1a05785e1a93568372908320b01cfd7402a42
Brendan Quinn <brendan-quinn@northwestern.edu>
2024-04-22 22:04:16 +0000

    Adds versioning to the pyramid and streamig buckets, with a lifecycle rule for 30 days that only applies to versions.

--- infrastructure/deploy/main.tf
+++ infrastructure/deploy/main.tf
@@ -218,7 +218,15 @@ resource "aws_s3_bucket" "meadow_streaming" {
   tags   = var.tags
 }
 
+resource "aws_s3_bucket_versioning" "meadow_streaming" {
+  bucket = aws_s3_bucket.meadow_streaming.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "aws_s3_bucket_lifecycle_configuration" "meadow_streaming" {
+  depends_on = [aws_s3_bucket_versioning.meadow_streaming]
   bucket = "${var.stack_name}-${var.environment}-streaming"
 
   rule {
@@ -228,6 +236,10 @@ resource "aws_s3_bucket_lifecycle_configuration" "meadow_streaming" {
 
     filter {
       prefix = ""
+    }
+    
+    noncurrent_version_expiration {
+      noncurrent_days = 30
     }
 
     transition {
@@ -251,6 +263,28 @@ resource "aws_s3_bucket_cors_configuration" "meadow_streaming" {
 
 data "aws_s3_bucket" "pyramid_bucket" {
   bucket = var.pyramid_bucket
+}
+
+resource "aws_s3_bucket_versioning" "pyramid_bucket" {
+  bucket = aws_s3_bucket.pyramid_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "meadow_pyramids" {
+  depends_on = [aws_s3_bucket_versioning.pyramid_bucket]
+  bucket = aws_s3_bucket.pyramid_bucket.id
+
+  rule {
+    id = "expire_old_versions"
+
+    noncurrent_version_expiration {
+      noncurrent_days = 30
+    }
+
+    status = "Enabled"
+  }
 }
 
 data "aws_s3_bucket" "digital_collections_bucket" {
