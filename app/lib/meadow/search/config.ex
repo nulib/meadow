9a617402b4f1c2c80c2c8eaa042c6c650df3b305
Michael B. Klein <mbklein@gmail.com>
2024-07-31 21:50:47 +0000

    Dynamically determine dimensionality of embedding field

--- app/lib/meadow/search/config.ex
+++ app/lib/meadow/search/config.ex
@@ -2,8 +2,11 @@ defmodule Meadow.Search.Config do
   @moduledoc """
   Convenience methods for retrieving search-specific configuration
   """
+  alias Meadow.Search.HTTP
 
   require Logger
+
+  @default_embedding_dimensions 384
 
   def index_configs do
     Application.get_env(:meadow, Meadow.Search.Cluster)
@@ -38,6 +41,7 @@ defmodule Meadow.Search.Config do
         File.read!(settings)
         |> Jason.decode!()
         |> put_in(["settings", "default_pipeline"], pipeline)
+        |> add_embedding_dimension()
 
       {%{settings: settings}, _} ->
         File.read!(settings) |> Jason.decode!()
@@ -50,6 +54,45 @@ defmodule Meadow.Search.Config do
   def embedding_model_id do
     Application.get_env(:meadow, Meadow.Search.Cluster)
     |> Keyword.get(:embedding_model_id)
+  end
+
+  def add_embedding_dimension(
+        %{"mappings" => %{"properties" => %{"embedding" => %{"dimension" => _}}}} = settings
+      ) do
+    settings
+    |> put_in(
+      ["mappings", "properties", "embedding", "dimension"],
+      embedding_model_dimensions(embedding_model_id())
+    )
+  end
+
+  def add_embedding_dimension(settings), do: settings
+
+  def embedding_model_dimensions(nil) do
+    Logger.warning(
+      "No embedding model id found in config, using default of #{@default_embedding_dimensions} dimensions"
+    )
+
+    @default_embedding_dimensions
+  end
+
+  def embedding_model_dimensions(model_id) do
+    case ["_plugins", "_ml", "_predict", "text_embedding", model_id]
+         |> HTTP.post!(%{text_docs: ["How many dimensions?"]}, [], recv_timeout: 30_000)
+         |> Map.get(:body)
+         |> get_in(["inference_results", Access.at(0), "output", Access.at(0), "data"]) do
+      nil ->
+        Logger.warning(
+          "Unable to determine model dimensions, using default of #{@default_embedding_dimensions}"
+        )
+
+        @default_embedding_dimensions
+
+      embedding ->
+        dims = length(embedding)
+        Logger.info("Model #{model_id} generates a #{dims}-dimension embedding")
+        dims
+    end
   end
 
   def index_versions do
