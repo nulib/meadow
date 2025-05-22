12200096e03a061789b04236abf39e849656e1fd
Brendan Quinn <brendan-quinn@northwestern.edu>
2025-05-22 16:03:52 +0000

    Make bulk size dynamic based on byte size to avoid OpenSearch request body limits
    Co-Authored-By: Karen Shaw <karendid@gmail.com>
    Co-Authored-By: Michael B. Klein <mbklein@gmail.com>

--- app/test/meadow/search/bulk_test.exs
+++ app/test/meadow/search/bulk_test.exs
@@ -8,6 +8,8 @@ defmodule Meadow.Search.BulkTest do
   alias Meadow.Search.{Bulk, Document, Index}
   alias Meadow.Search.Config, as: SearchConfig
 
+  import ExUnit.CaptureLog
+
   @target_version 2
   @target_index SearchConfig.alias_for(Work, @target_version)
 
@@ -41,5 +43,39 @@ defmodule Meadow.Search.BulkTest do
     Index.refresh(@target_index)
 
     assert indexed_doc_count(Work, @target_version) == {:ok, work_count + 1}
+  end
+
+  describe "large document bulks" do
+    setup do
+      cluster_config = Application.get_env(:meadow, Meadow.Search.Cluster)
+      updated_config = Keyword.put(cluster_config, :bulk_request_limit, 2_500)
+      Application.put_env(:meadow, Meadow.Search.Cluster, updated_config)
+
+      on_exit(fn ->
+        Application.put_env(:meadow, Meadow.Search.Cluster, cluster_config)
+      end)
+    end
+
+    test "upload/2 with large documents", %{work_count: work_count} do
+      assert indexed_doc_count(Work, @target_version) == {:ok, work_count}
+
+      document_1 =
+        work_fixture()
+        |> Repo.preload(Work.required_index_preloads())
+        |> Document.encode(@target_version)
+
+      document_2 =
+        work_fixture()
+        |> Repo.preload(Work.required_index_preloads())
+        |> Document.encode(@target_version)
+
+      assert capture_log(fn ->
+        Bulk.upload([document_1, document_2], @target_index)
+      end) =~ "Uploading 1 document ("
+
+      Index.refresh(@target_index)
+
+      assert indexed_doc_count(Work, @target_version) == {:ok, work_count + 2}
+    end
   end
 end
