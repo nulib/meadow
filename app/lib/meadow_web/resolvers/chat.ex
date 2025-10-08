6b5e1c0a203b7c125c4cf2f811a99c72887758a0
Karen Shaw <karendid@gmail.com>
2025-10-08 19:12:08 +0000

    Remove editor auth from websocket

--- app/lib/meadow_web/resolvers/chat.ex
+++ app/lib/meadow_web/resolvers/chat.ex
@@ -12,11 +12,12 @@ defmodule MeadowWeb.Resolvers.Chat do
       type: "plan",
     }
 
-    Absinthe.Subscription.publish(
+
+   Absinthe.Subscription.publish(
       MeadowWeb.Endpoint,
       response,
       chat_response: "conversation:#{conversation_id}"
-    )
+   )
 
     {:ok, %{conversation_id: conversation_id, type: type, query: query, prompt: prompt}}
   end
