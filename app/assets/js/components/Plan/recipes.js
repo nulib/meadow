68b091bc26578a6268d0d14d23c6b306ba6ea21e
Karen Shaw <karendid@gmail.com>
2025-10-22 15:06:14 +0000

    Add recipes to AI edit UI

---
+++ app/assets/js/components/Plan/recipes.js
@@ -0,0 +1,5 @@
+export const recipes = [
+  "Add a description to the work based on the visual content of the image",
+  "Add, update, or replace subject headings as appropriate",
+  "Fill out controlled fields if possible",
+];
