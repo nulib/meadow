16f477f79d3b03e472f47f4c9370011e4695e7d6
Michael B. Klein <mbklein@gmail.com>
2025-10-24 02:48:08 +0000

    Configure IIIF image tool with auth headers

--- app/priv/python/agent/src/meadow_metadata_agent/__init__.py
+++ app/priv/python/agent/src/meadow_metadata_agent/__init__.py
@@ -1,9 +1,7 @@
 """Public exports for the meadow_metadata_agent package."""
 
 from .execute import query_claude
-from .initialize import image_fetcher
 
 __all__ = [
     "query_claude",
-    "image_fetcher",
 ]
