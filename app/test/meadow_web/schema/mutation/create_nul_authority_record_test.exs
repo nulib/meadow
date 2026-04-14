767a287105caee9a8ab2a2157604103ffb27a258
Michael B. Klein <mbklein@gmail.com>
2026-04-14 15:58:22 +0000

    Add '_test' to test file names that didn't have it (credo issue) Remove GetGroupMembers query and test since it's not used any more

---
+++ app/test/meadow_web/schema/mutation/create_nul_authority_record_test.exs
@@ -0,0 +1,40 @@
+defmodule MeadowWeb.Schema.Mutation.CreateNULAuthorityRecordTest do
+  use Meadow.DataCase
+  use MeadowWeb.ConnCase, async: false
+  use Wormwood.GQLCase
+
+  load_gql(MeadowWeb.Schema, "test/gql/CreateNULAuthorityRecord.gql")
+
+  test "should be a valid mutation" do
+    {:ok, result} =
+      query_gql(
+        variables: %{"label" => "test label", "hint" => "test hint"},
+        context: gql_context()
+      )
+
+    assert "test label" == get_in(result, [:data, "createNULAuthorityRecord", "label"])
+    assert "test hint" == get_in(result, [:data, "createNULAuthorityRecord", "hint"])
+  end
+
+  describe "authorization" do
+    test "viewers and editors are not authorized to create NUL AuthorityRecords" do
+      {:ok, result} =
+        query_gql(
+          variables: %{"label" => "test label", "hint" => "test hint"},
+          context: gql_context(%{role: :editor})
+        )
+
+      assert %{errors: [%{message: "Forbidden", status: 403}]} = result
+    end
+
+    test "managers and above are authorized to create NUL AuthorityRecords" do
+      {:ok, result} =
+        query_gql(
+          variables: %{"label" => "test label", "hint" => "test hint"},
+          context: gql_context(%{role: :manager})
+        )
+
+      assert result.data["createNULAuthorityRecord"]
+    end
+  end
+end
---
+++ app/test/meadow_web/schema/query/get_preservation_checks_test.exs
@@ -0,0 +1,26 @@
+defmodule MeadowWeb.Schema.Query.PreservationChecksTest do
+  use Meadow.DataCase
+  use MeadowWeb.ConnCase, async: false
+  use Wormwood.GQLCase
+
+  alias Meadow.Data.PreservationChecks
+
+  load_gql(MeadowWeb.Schema, "test/gql/GetPreservationChecks.gql")
+
+  test "should be a valid query" do
+    PreservationChecks.create_job(%{})
+    PreservationChecks.create_job(%{})
+    PreservationChecks.create_job(%{})
+
+    result =
+      query_gql(
+        variables: %{},
+        context: gql_context()
+      )
+
+    assert {:ok, query_data} = result
+
+    preservation_checks = get_in(query_data, [:data, "preservationChecks"])
+    assert length(preservation_checks) == 3
+  end
+end
