451a0e14b8ce8df96b26183a6d6ff8f09aaeeb77
Karen Shaw <karendid@gmail.com>
2026-04-17 20:03:15 +0000

    Redeploy cost estimate migration

--- app/priv/repo/migrations/20260410165018_add_ai_preview_to_ingest_sheets.exs
+++ app/priv/repo/migrations/20260410165018_add_ai_preview_to_ingest_sheets.exs
@@ -3,8 +3,8 @@ defmodule Meadow.Repo.Migrations.AddAiPreviewToIngestSheets do
 
   def change do
     alter table(:ingest_sheets) do
-      # add :ai_cost_actual, :float, null: false, default: 0.0
-      # add :ai_cost_estimate, :float, null: false, default: 0.0
+      add :ai_cost_actual, :float, null: false, default: 0.0
+      add :ai_cost_estimate, :float, null: false, default: 0.0
       add :ai_preview, {:array, :map}, default: [], null: false
     end
   end
