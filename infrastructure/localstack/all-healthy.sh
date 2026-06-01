7ccb90220d2b9211453d11ea04c8f638b61b3f6c
Michael B. Klein <mbklein@gmail.com>
2026-06-01 16:02:39 +0000

    Add code to diagnose localstack startup issue

--- infrastructure/localstack/all-healthy.sh
+++ infrastructure/localstack/all-healthy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 compose_dir=$(dirname "$0")
+docker compose -f $compose_dir/docker-compose.yml ps
 statuses=$(docker compose -f $compose_dir/docker-compose.yml ps -q | xargs docker inspect --format='{{.State.Health.Status}}' 2>/dev/null)
 if [[ $(echo -n $statuses | wc -w) -lt 3 ]] || (echo $statuses | grep "starting\|unhealthy" > /dev/null); then
   echo "One or more services are not healthy"
